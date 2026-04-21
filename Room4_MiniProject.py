3420ee19dbc5521b0d42fe08c9a707bd6c1528a4
Zakk-Khan <zakir.khan124@outlook.com>
2026-04-21 13:48:19 +0100

    Add files via upload

---
+++ Room4_MiniProject.py
@@ -0,0 +1,36 @@
+product_list = ["Coffee", "Tea", "Sugar", "Milk"]
+products = ["Coffee", "Tea", "Sugar", "Milk"]
+print("Welcome to Cool Cafe, please select option '1' for products menu or '0' to exit") 
+
+def main_menu(): # 1 FOR PRODUCT MENU 0 TO EXIT  - Zak
+    print("/n---Main Menu---")
+    print("1. Products Menu")
+    print("0. Exit") 
+
+def product_menu(): #SELECT A RANGE OF OPTIONS 1. PRINT PRODUCT LIST 2. ADD PRODUCT 3.UPDATE PRODUCT 4.REMOVE PRODUCT. AND 0 TO RETURN TO MAIN MENU - Mukarram / Demi
+    print("/n---Products Menu---")
+    print("1. Product List")
+    print("2. Add Products")
+    print("3. Update Product")
+    print("4. Delete Product")
+    print("5. Back to Main Menu")
+
+    product_list = ["Coffee", "Tea", "Sugar", "Milk"]
+
+def add_product():  # ADD NEW PRODUCTS TO A LIST - Zak
+    products = input("Select 1. Add products to Product List")
+if  products == 1.:
+    name = input("Enter new product name")
+    product_list.append(name)
+    print(f"Added {name} to products")
+
+elif print("Invalid option no products added"):
+
+
+    def remove_product():# REMOVE PRODUCTS FROM A LIST USING INDEX NUMBER - Zak 
+     print()# consider exception handling 
+   
+    
+
+
+main_menu()
