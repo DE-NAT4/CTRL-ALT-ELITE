5268757a41a87deada34057b044601a08cbb7270
Dilrukshi-cake <1315181@genstudents.org>
2026-04-25 01:22:05 +0100

    checking on Sprint 3

--- CTRL-ALT-ELITE/Room4_MiniProject.py
+++ CTRL-ALT-ELITE/Room4_MiniProject.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:CTRL-ALT-ELITE/Room4_MiniProject.py
 product_list = ["Coffee", "Tea", "Sugar", "Milk"]
 products = ["Coffee", "Tea", "Sugar", "Milk"]
 print("Welcome to Cool Cafe, please select option '1' for products menu or '0' to exit") 
@@ -41,3 +42,48 @@ elif print("Invalid option no products added"):
 
 
 main_menu()
+=======
+product_list = ["Coffee", "Tea", "Sugar", "Milk"]
+products = ["Coffee", "Tea", "Sugar", "Milk"]
+print("Welcome to Cool Cafe, please select option '1' for products menu or '0' to exit") 
+
+def main_menu(): # 1 FOR PRODUCT MENU 0 TO EXIT  - Zak
+    print("/n---Main Menu---")
+    print("1. Products Menu")
+    print("0. Exit") 
+
+def product_menu(): #SELECT A RANGE OF OPTIONS 1. PRINT PRODUCT LIST 2. ADD PRODUCT 3.UPDATE PRODUCT 4.REMOVE PRODUCT. AND 0 TO RETURN TO MAIN MENU - Mukarram / Demi
+    print("/n---Products Menu---")
+    print("1. Product List")
+    print("2. Add Products")
+    print("3. Update Product")
+    print("4. Delete Product")
+    print("5. Back to Main Menu")
+
+    product_list = ["Coffee", "Tea", "Sugar", "Milk"]
+
+def add_product():  # ADD NEW PRODUCTS TO A LIST - Zak
+    products = input("Select 1. Add products to Product List")
+if  products == 1.:
+    name = input("Enter new product name")
+    product_list.append(name)
+    print(f"Added {name} to products")
+
+elif print("Invalid option no products added"):
+
+
+    def remove_product():# REMOVE PRODUCTS FROM A LIST USING INDEX NUMBER - Zak 
+     print()# consider exception handling 
+
+
+
+     #happy hour
+     #make a change
+     #testingt
+
+   
+    
+
+
+main_menu()
+>>>>>>> 22932bc64c25533521b405a0c82390e620b4e5f9:Room4_MiniProject.py
